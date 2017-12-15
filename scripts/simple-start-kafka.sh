c6224272183a95b755363e472e4b27b689f2f491
Yazan Obeidi <yazan@protonmail.ch>
2017-12-15 17:52:30 -0500

    fixing new bugs: newline to auto_create_topics, update kafka

--- scripts/simple-start-kafka.sh
+++ scripts/simple-start-kafka.sh
@@ -11,7 +11,7 @@ done
 # Enable/disable auto creation of topics
 if [ ! -z "$AUTO_CREATE_TOPICS" ]; then
     echo "auto.create.topics.enable: $AUTO_CREATE_TOPICS"
-    echo "auto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
+    echo "\nauto.create.topics.enable=$AUTO_CREATE_TOPICS" >> $KAFKA_HOME/config/server.properties
 fi
 
 if [ ! -z "$DELETE_TOPIC_ENABLE" ]; then
